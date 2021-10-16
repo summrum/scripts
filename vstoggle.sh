f13acbda3f74975f707c0641f2e1f09beec621e0
summrum <92579165+summrum@users.noreply.github.com>
2021-10-16 19:23:32 +0100

    Update vstoggle.sh

--- vstoggle.sh
+++ vstoggle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script to enable and disable V-Sync on intel GPU for current user
-# v:1.4 2021-10-16
+# v:1.5 2021-10-16
 
 scriptname='vstoggle'
-scriptver='1.4'
+scriptver='1.5'
 
 usage() {
 	cat <<EOF
@@ -43,13 +43,30 @@ exit 0
 fi
 }
 
+dsn="<device screen=\"0\" driver=\"dri2\">"
+vbn="<option name=\"vblank_mode\" value=\"0\"\/>"
+dsy="<device screen=\"0\" driver=\"dri3\">"
+vby="<option name=\"vblank_mode\" value=\"3\"\/>"
+
 if test -f ~/.drirc; then
-    if [ ! -z "$(grep "option name=\"vblank_mode\" value=\"0\"" ~/.drirc)" ]; then
+    if [ ! -z "$(grep "device screen=\"1\"" ~/.drirc)" ]; then
+    echo "vstoggle is not currently designed to work with multiple monitors"
+    exit 1
+    elif [ ! -z "$(grep "option name=\"vblank_mode\" value=\"0\"" ~/.drirc)" ]; then
     vstat=0
     else
     vstat=1
     fi
-    else
+else
+    cat >> ~/.drirc << EOF
+<driconf>
+<device screen="0" driver="dri3">
+<application name="Default">
+<option name="vblank_mode" value="3"/>
+</application>
+</device>
+</driconf>
+EOF
     vstat=1
 fi
 
@@ -60,29 +77,17 @@ cmd() {
 		if [ "$vstat" -eq 1 ]; then
         echo -e "${YELLOW}V-Sync is already on"
         exit 1
-        else # FIXME THIS SHOULD IDEALLY BE CHANGED TO MATCH THE ENTIRE MULTI-LINE PATTERN
-        sed -i /"<driconf>"/d ~/.drirc
-        sed -i /"<device screen=\"0\" driver=\"dri2\">"/d ~/.drirc
-        sed -i /"<application name=\"Default\">"/d ~/.drirc
-        sed -i /"<option name=\"vblank_mode\" value=\"0\"\/>"/d ~/.drirc
-        sed -i /"<\/application>"/d ~/.drirc
-        sed -i /"<\/device>"/d ~/.drirc
-        sed -i /"<\/driconf>"/d ~/.drirc
+        else # FIXME
+        sed -i s/"$dsn"/"$dsy"/g ~/.drirc
+        sed -i s/"$vbn"/"$vby"/g ~/.drirc
         echo -e "${GREEN}V-Sync now switched on.\n${NC}Restart to apply changes."
         exit 0
         fi
 	elif (( USE_NO )); then
 		if [ "$vstat" -eq 1 ]
-        then
-        cat >> ~/.drirc << EOF
-<driconf>
-<device screen="0" driver="dri2">
-<application name="Default">
-<option name="vblank_mode" value="0"/>
-</application>
-</device>
-</driconf>
-EOF
+        then # FIXME
+        sed -i s/"$dsy"/"$dsn"/g ~/.drirc
+        sed -i s/"$vby"/"$vbn"/g ~/.drirc
         echo -e "${RED}V-Sync now switched off.\n${NC}Restart to apply changes."
         exit 0
         else
